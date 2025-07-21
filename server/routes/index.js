0c91643a69845a45f8f492d259b6aec0f6403ac0
HORTH2025 <horth.heng@student.passerellesnumeriques.org>
2025-07-21 10:43:27 +0700

    feat(borrow-book):create Api borrow book and Crud API [v2-118]

--- server/routes/index.js
+++ server/routes/index.js
@@ -4,11 +4,13 @@ const apiResource = require('./ApiRescource');
 
 const bookController = require('../controllers/bookController');
 const categoriesController = require('../controllers/categoriesController');
+const borrowController = require('../controllers/borrowController');
 
 
 const resources = [
   apiResource('/books', bookController),
   apiResource('/categories', categoriesController),
+  apiResource('/borrow', borrowController),
 ];
 
 resources.forEach(resource => {
