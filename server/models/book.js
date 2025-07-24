8e7edce0f4136f00f856b9262ef3b16078a802d1
phouk-sophean <phouksophean36@gmail.com>
2025-07-24 08:33:01 +0700

    resolve conflic

--- server/models/book.js
+++ server/models/book.js
@@ -7,10 +7,13 @@ module.exports = (sequelize, DataTypes) => {
     donated_by: DataTypes.STRING,
     public_year: DataTypes.INTEGER,
     description: DataTypes.TEXT,
-    language: DataTypes.STRING,
     available: {
       type: DataTypes.BOOLEAN,
       defaultValue: true,
-    }
+    },
+    // language_id: {
+    //   type: DataTypes.INTEGER,
+    //   allowNull: false,
+    // },
   });
 };
